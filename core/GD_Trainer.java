6e271180f3ef2b24fe0a5087638fd81c9eb54e7b
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-05 11:02:27 +0800

    Finished lineSearch(). Working on update(), a helper for lineSearch(), for updating current params by descending by gradient direction

--- core/GD_Trainer.java
+++ core/GD_Trainer.java
@@ -1,15 +1,24 @@
 package core;
 
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 import defs.Dataset;
 import defs.Hypers;
 
 public class GD_Trainer {
 	
-	int numTopic;
+	private static final double EPSILON = 1;
+	private static final double ALPHA = 0.5;
+	private static final double GAMMA = Math.pow(10, -4);
+	private static final double EPSILON_STEP = Math.pow(10, -10);
+	
 	Dataset ds;
+	// settings of this trainer
+	int numTopic;
 	Hypers hypers;
+	private int maxIter;
+	private double stepSize;
 	
 	/**
 	 * 
@@ -17,14 +26,86 @@ public class GD_Trainer {
 	 * @param resDir
 	 * @return local optimal parameters which give a local minimum of the objective function (minimum errors + regularizers)
 	 */
-	private Parameters gradDescent(Parameters initParams, String resDir) {
+	Parameters gradDescent(Parameters initParams, String resDir) {
+		
 		// TODO Auto-generated method stub
-		
-		GradCalculator gradCal = new GradCalculator(this);
-		
 		Parameters cParams = new Parameters(initParams);
 		double cValue = objValue(initParams);
-		double difference = 100.0;
+		double difference = Double.POSITIVE_INFINITY;
+		int numIter = 0;
+		
+		GradCalculator gradCal = new GradCalculator(this);
+		// while not convergence and still can try more
+		while ( (Math.abs(difference) > EPSILON) && (numIter < maxIter) ) {
+			numIter ++;
+			Parameters cGrad = gradCal.calGrad(cParams);
+			Parameters nParams = lineSearch(cParams, cGrad, cValue);
+			double nValue = objValue(nParams);
+			
+			difference = nValue - cValue;
+			// prep for next iter
+			cParams = new Parameters(nParams);						
+			cValue = nValue;
+		}
+		
+		return cParams;
+	}
+	
+	private Parameters lineSearch(Parameters cParams, Parameters cGrad, double cValue) {
+		
+		stepSize = 1/ALPHA;
+		Parameters nParams = new Parameters(cParams);
+		boolean sufficentReduction = false;
+		
+		while (!sufficentReduction && (stepSize > EPSILON_STEP)) {
+			stepSize = stepSize * ALPHA;
+			nParams = update(cParams, stepSize, cGrad);
+			// todo: may need some projection here to guarantee some constraints
+			double funcDiff = objValue(nParams) - cValue;
+			double sqDiff = sqDiff(nParams, cParams);
+			double reduction = - GAMMA/stepSize * sqDiff;
+			sufficentReduction = (funcDiff < reduction);
+			
+			if (funcDiff == 0) {
+				System.out.println("meet a local minimum !!!");
+				return nParams;
+			}
+		}
+		
+		if (sufficentReduction) {
+			System.out.println("found new params with sufficient reduction");
+			return nParams;
+		} else {
+			System.out.println("Cannot find new params with sufficient reduction. "
+					+ "Line search stopped due to step size too small");
+			return cParams;
+		}
+	}
+
+	private double sqDiff(Parameters p1, Parameters p2) {
+		
+		double sq_diff = square(p1.topicUser.subtract(p2.topicUser).getFrobeniusNorm());
+		sq_diff += square(p1.topicItem.subtract(p2.topicItem).getFrobeniusNorm());
+		sq_diff += square(p1.brandUser.subtract(p2.brandUser).getFrobeniusNorm());
+		sq_diff += square(p1.brandItem.subtract(p2.brandItem).getFrobeniusNorm());
+		for (int u = 0; u < ds.numUser; u++) {
+			sq_diff += square(p1.userDecisionPrefs[u] - p2.userDecisionPrefs[u]);
+		}
+		
+		return sq_diff;
+	}
+
+	private Parameters update(Parameters cParams, double stepSize, Parameters cGrad) {
+		// TODO Auto-generated method stub
+		Parameters nParams = new Parameters(ds.numUser, ds.numItem, numTopic, ds.numBrand);
+		for (int u = 0; u < ds.numUser; u++) {
+			RealVector curTopicFeat = cParams.topicUser.getColumnVector(u);
+			RealVector descentGrad = cGrad.topicUser.getColumnVector(u).mapMultiply(-stepSize);
+			RealVector nextTopicFeat = curTopicFeat.add(descentGrad);
+			nParams.topicUser.setColumnVector(u, nextTopicFeat);
+		}
+		
+		return null;
 	}
 
 	private double objValue(Parameters params) {
