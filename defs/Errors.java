49bae8356741fee1415ccab288b9b6a2a58f0c29
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-12 16:50:46 +0800

    Add cls Errors

---
+++ defs/Errors.java
@@ -0,0 +1,18 @@
+package defs;
+
+public class Errors {
+	public double topicUser;
+	public double topicItem;
+	public double brandUser;
+	public double brandItem;
+	public double decisionPrefs;
+	
+	public Errors(double topicUser, double topicItem, double brandUser, double brandItem, double decisionPrefs) {
+		super();
+		this.topicUser = topicUser;
+		this.topicItem = topicItem;
+		this.brandUser = brandUser;
+		this.brandItem = brandItem;
+		this.decisionPrefs = decisionPrefs;
+	}
+}
