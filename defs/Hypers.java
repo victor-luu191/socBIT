400e952cf56ba868f0b7732b24a76629de475583
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-19 15:14:11 +0800

    Add fusing hyper-param alpha of STE model. Thus, add new setters to distinguish hyper constructors by different models.

--- defs/Hypers.java
+++ defs/Hypers.java
@@ -7,12 +7,26 @@ public class Hypers {
 	public double weightLambda;
 	public double decisionLambda;
 	
+	public double alpha;	// tuning param only for STE, alpha controls how much a user believe in his own ratings vs. friend's ratings 
+	
+	public Hypers(double topicLambda, double alpha) {
+		this.topicLambda = topicLambda;
+		this.alpha = alpha;
+	}
+
 	public Hypers(double topicLambda, double brandLambda, double weightLambda, double decisionLambda) {
-		super();
+		
 		this.topicLambda = topicLambda;
 		this.brandLambda = brandLambda;
 		this.weightLambda = weightLambda;
 		this.decisionLambda = decisionLambda;
 	}
+
+	public static Hypers setBySocBIT(double topicLambda, double brandLambda, double weightLambda, double decisionLambda) {
+		return new Hypers(topicLambda, brandLambda, weightLambda, decisionLambda);
+	}
 	
+	public static Hypers setBySTE(double topicLambda, double alpha) {
+		return new Hypers(topicLambda, alpha);
+	}
 }
