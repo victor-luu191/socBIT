b5c22e2ead7cfdf1ceb098a414e29cd334d2fd56
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-24 17:05:02 +0800

    performed safeguard against NaNs in logistic func and its diff. NaNs are caused by too large exponents in these functions.

--- helpers/UtilFuncs.java
+++ helpers/UtilFuncs.java
@@ -5,30 +5,49 @@ import org.apache.commons.math3.linear.RealMatrix;
 
 public class UtilFuncs {
 	
+	private static final double LOWER = -50;
+	private static final double UPPER = 50;
+
 	static double logistic(double x) {
 		return 1/(1 + Math.exp(-x));
 	}
 	
 	public static double logisDiff(double x) {
 		double invExp = Math.exp(-x);
-		return invExp/Math.pow(1 + invExp, 2);
+		double value = invExp/square(1 + invExp);
+		//XXX: value can be NaN if the square(1 + invExp) is too large
+		// in that case the value  actually converges 0
+		// we guard against NaN by returning 0 instead
+		if (!Double.isNaN(value)) {
+			return value;
+		} else {
+			return 0;
+		}
 	}
 
-	static RealMatrix logisticMat(RealMatrix matrix) {
+	public static RealMatrix cutoff(RealMatrix matrix) {
 		int rowDim = matrix.getRowDimension();
 		int colDim = matrix.getColumnDimension();
-		RealMatrix logisMatrix = new Array2DRowRealMatrix(rowDim, colDim);
+		
+		RealMatrix cutoffMatrix = new Array2DRowRealMatrix(rowDim, colDim);
 		for (int i = 0; i < rowDim; i++) {
 			for (int j = 0; j < colDim; j++) {
-				logisMatrix.setEntry(i, j, logistic(matrix.getEntry(i, j)));
+				double entry = matrix.getEntry(i, j);
+				double logisticValue = logistic(entry);
+				if (!Double.isNaN(logisticValue)) {
+					cutoffMatrix.setEntry(i, j, logisticValue);
+				} else {
+					if (entry < LOWER) {
+						cutoffMatrix.setEntry(i, j, 0);
+					}
+					if (entry > UPPER) {
+						cutoffMatrix.setEntry(i, j, 1);
+					}
+				}
 			}
 		}
 		
-		return logisMatrix;
-	}
-	
-	public static RealMatrix bound(RealMatrix matrix) {
-		return logisticMat(matrix);
+		return cutoffMatrix;
 	}
 
 	public static double square(double d) {
