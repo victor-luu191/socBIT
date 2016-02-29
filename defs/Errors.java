2e5bf361de36f9e1f1b05a62667c9f6a2222756c
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-29 13:33:36 +0800

    successfully used Optional type to handle cases when brand-related errors are not present

--- defs/Errors.java
+++ defs/Errors.java
@@ -1,18 +1,39 @@
 package defs;
 
+import java.util.Optional;
+
 public class Errors {
 	public double topicUser;
 	public double topicItem;
-	public double brandUser;
-	public double brandItem;
-	public double decisionPrefs;
 	
-	public Errors(double topicUser, double topicItem, double brandUser, double brandItem, double decisionPrefs) {
-		super();
+	public Double brandUser;
+	public Double brandItem;
+	public Double decisionPrefs;
+	
+	public Errors(double topicUser, double topicItem, Optional<Double> maybe_brandUser, Optional<Double> maybe_brandItem,
+					Optional<Double> maybe_decisionPrefs) {
+		
 		this.topicUser = topicUser;
 		this.topicItem = topicItem;
-		this.brandUser = brandUser;
-		this.brandItem = brandItem;
-		this.decisionPrefs = decisionPrefs;
+		this.brandUser = maybe_brandUser.orElse(Double.NaN);
+		this.brandItem = maybe_brandItem.orElse(Double.NaN);
+		this.decisionPrefs = maybe_decisionPrefs.orElse(Double.NaN);
 	}
+
+	@Override
+	public String toString() {
+		return  topicUser + ", " + topicItem + ", " + brandUser + ", " + brandItem + ", " + decisionPrefs ;
+	}
+
+	
+	
+//	public Errors(double topicUser, double topicItem) {
+//		
+//		this.topicUser = topicUser;
+//		this.topicItem = topicItem;
+//		
+//		brandUser = Optional.empty();
+//		brandItem = Optional.empty();
+//		decisionPrefs = Optional.empty();
+//	}
 }
