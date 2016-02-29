78513c2b79f1bb36ef617edc24f4e2bad8e50691
Minh Duc <mdluu.2011@smu.edu.sg>
2016-02-29 20:14:35 +0800

    renamed resDir to dir

--- helpers/ParamSaver.java
+++ helpers/ParamSaver.java
@@ -9,36 +9,36 @@ import core.SocBIT_Params;
 
 public class ParamSaver {
 	
-	public static void save(Params params, String resDir) throws IOException {
+	public static void save(Params params, String dir) throws IOException {
 		
 		if (params instanceof SocBIT_Params) {
-			saveTopicComponents(params, resDir);
-			saveBrandComponents(params, resDir);
+			saveTopicComponents(params, dir);
+			saveBrandComponents(params, dir);
 		}
 		else {
-			saveTopicComponents(params, resDir);
+			saveTopicComponents(params, dir);
 		}
 	}
 
-	private static void saveBrandComponents(Params params, String resDir)
+	private static void saveBrandComponents(Params params, String dir)
 			throws IOException {
 		SocBIT_Params castParams = (SocBIT_Params) params;
-		String userBrandFeat_file = resDir + "user_brand_feats.csv";
+		String userBrandFeat_file = dir + "user_brand_feats.csv";
 		Savers.save(castParams.brandUser.toString(), userBrandFeat_file);
 		
-		String itemBrandFeat_file = resDir + "item_brand_feats.csv";
+		String itemBrandFeat_file = dir + "item_brand_feats.csv";
 		Savers.save(castParams.brandItem.toString(), itemBrandFeat_file);
 		
-		String decisionPref_file = resDir + "decision_prefs.csv";
+		String decisionPref_file = dir + "decision_prefs.csv";
 		Savers.save(Arrays.toString(castParams.userDecisionPrefs), decisionPref_file);
 	}
 
-	private static void saveTopicComponents(Params params, String resDir)
+	private static void saveTopicComponents(Params params, String dir)
 			throws IOException {
-		String userTopicFeat_file = resDir + "user_topic_feats.csv";
+		String userTopicFeat_file = dir + "user_topic_feats.csv";
 		Savers.save(params.topicUser.toString(), userTopicFeat_file);
 		
-		String itemTopicFeat_file = resDir + "item_topic_feats.csv";
+		String itemTopicFeat_file = dir + "item_topic_feats.csv";
 		Savers.save(params.topicItem.toString(), itemTopicFeat_file);
 	}
 }
