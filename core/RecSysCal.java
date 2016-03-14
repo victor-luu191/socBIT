f86e0069098f07614da68defd1d865ba9120d69f
Minh Duc <mdluu.2011@smu.edu.sg>
2016-03-14 15:54:29 +0800

    created a global field in RecSysCal.java for storing estimated ratings st subclasses of RecSysCal can reuse the field

--- core/RecSysCal.java
+++ core/RecSysCal.java
@@ -1,14 +1,22 @@
 package core;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
+import defs.Dataset;
 import defs.Params;
 
 public abstract class RecSysCal {
 	
+	protected RealMatrix estimated_ratings;
+	
+	public RecSysCal(Dataset ds) {
+		estimated_ratings = new Array2DRowRealMatrix(ds.numUser, ds.numItem);
+	}
+	
 	abstract double objValue(Params params);
 	
-	abstract RealMatrix estRatings(Params params);
+	abstract void estRatings(Params params);
 	
 	abstract RealMatrix calRatingErrors(Params params);
 }
