010d3fd9f19378052f8bbb968270d493c652f87b
Minh Duc <mdluu.2011@smu.edu.sg>
2016-03-15 11:44:55 +0800

    include bSTE in valid models

--- defs/InvalidModelException.java
+++ defs/InvalidModelException.java
@@ -5,7 +5,7 @@ public class InvalidModelException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidModelException() {
-		String msg = "Invalid model. Current implementation only accepts either socBIT or STE model. "
+		String msg = "Invalid model. Current implementation only accepts one of three models: socBIT, STE or bSTE. "
 				+ "Pls type or copy the correct model name next time.";
 		
 		System.out.println(msg);
