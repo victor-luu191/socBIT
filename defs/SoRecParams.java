b05f2a282a98f53cf82002500a66b693551226ee
Minh Duc <mdluu.2011@smu.edu.sg>
2016-03-24 17:55:37 +0800

    added uniform inits and sqDiff() to cls SoRecParams

--- defs/SoRecParams.java
+++ defs/SoRecParams.java
@@ -1,7 +1,10 @@
 package defs;
 
+import helpers.UtilFuncs;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 public class SoRecParams extends Params {
 
@@ -9,12 +12,29 @@ public class SoRecParams extends Params {
 	
 	public SoRecParams(int numUser, int numItem, int numTopic) {
 		super(numUser, numItem, numTopic);
+		initUserTopicFeats(numUser, numTopic);
+		initItemTopicFeats(numItem, numTopic);
+		
+		initUnifZ(numUser, numTopic);
+	}
+
+	private void initUnifZ(int numUser, int numTopic) {
 		zMatrix = new Array2DRowRealMatrix(numTopic, numUser);
+		RealVector uniformVector = uniformVector(numTopic);
+		for (int u = 0; u < numUser; u++) {
+			zMatrix.setColumnVector(u, uniformVector);
+		}
 	}
 
 	public SoRecParams(SoRecParams params) {
 		super(params);
 		this.zMatrix = params.zMatrix;
+	}
+
+	public double sqDiff(SoRecParams other) {
+		double topicDiff = topicDiff(other);
+		double zDiff = UtilFuncs.sqFrobNorm(this.zMatrix.subtract(other.zMatrix));
+		return topicDiff + zDiff;
 	}
 
 }
