13377d9e89ee4bb39a39c363f7056943db2d8485
Minh Duc <mdluu.2011@smu.edu.sg>
2016-03-24 09:52:19 +0800

    add def for params of SoRec

---
+++ defs/SoRecParams.java
@@ -0,0 +1,15 @@
+package defs;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+public class SoRecParams extends Params {
+
+	public RealMatrix zMatrix;
+	
+	public SoRecParams(int numUser, int numItem, int numTopic) {
+		super(numUser, numItem, numTopic);
+		zMatrix = new Array2DRowRealMatrix(numTopic, numUser);
+	}
+
+}
